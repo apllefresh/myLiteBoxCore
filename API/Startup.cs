8d0595899b2836e6330f324ff8ae7cbeca5698b7
Konstantin Alekseev <kalexeev@gotrg.com>
2018-11-05 23:01:16 +0300

    include React

--- API/Startup.cs
+++ API/Startup.cs
@@ -5,47 +5,54 @@ using BLL.DI;
 using DAL.DI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using React.AspNet;
 
 namespace API
 {
-       public class Startup
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
         {
-            public Startup(IConfiguration configuration)
-            {
-                Configuration = configuration;
-            }
-
-            public IConfiguration Configuration { get; }
-
-            public IServiceProvider ConfigureServices(IServiceCollection services)
-            {
-                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-                var builder = new ContainerBuilder();
-                builder.RegisterModule(new DALAutofacModule());
-                builder.RegisterModule(new BLLAutofacModule());
-                builder.Populate(services);
-                var container = builder.Build();
-                return new AutofacServiceProvider(container);
-            }
-
-            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-            {
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    app.UseHsts();
-                }
-
-                app.UseHttpsRedirection();
-                app.UseMvc();
-            }
+            Configuration = configuration;
         }
-    
+
+        public IConfiguration Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();            services.AddReact();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new DALAutofacModule());
+            builder.RegisterModule(new BLLAutofacModule());
+            builder.Populate(services);
+            var container = builder.Build();
+            return new AutofacServiceProvider(container);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseReact(config => { });
+            
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+
 }
--- DAL/Entities/InventoryDate.cs
+++ DAL/Entities/InventoryDate.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL.Entities
 {
+    [Table(name: "inventorydate")]
     public class InventoryDate
     {
-        public int id { get; set; }
-        public DateTime date { get; set; }
-        public DateTime dateget2price { get; set; }
+        [Column(name:"id")]
+        public int Id { get; set; }
+
+        [Column(name: "date")]
+        public DateTime Date { get; set; }
+
+        [Column(name: "dateget2price")]
+        public DateTime DateGet2Price { get; set; }
 
         IEnumerable<InventoryHead> inventoryHeads { get; set; }
     }
